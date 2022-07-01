af6a063b895193dd7983358f604f0d5866bcb549
Dipali <dipssonawane123@gmail.com>
2022-07-01 23:01:27 +0530

    Modified:Ability to deque element from list

--- src/com/stackandQueue/QueueUsingLinkedListMain.java
+++ src/com/stackandQueue/QueueUsingLinkedListMain.java
@@ -1,58 +1,82 @@
-/*
- * 1. ability to create queue ......uc3
- */
+ /*
+  * 1. ability to create queue ......uc3
+  * 2 . ability to deque from beginning.......uc4
+  */
 package com.stackandQueue;
 
 public class QueueUsingLinkedListMain {
     private Node front, rear;
-    private int currentSize; // number of items
+        private int currentSize;
 
-    //class to define linked node
-    private class Node {
-        int data;
-        Node next;
-    }
-
-    // create constructor name as QueueUsingLinkedListMain.
-    public QueueUsingLinkedListMain() {
-        front = null;
-        rear = null;
-        currentSize = 0;
-    }
-    /* create a method isEmpty
-     * they are boolean type that means Returns true if the queue is empty, else false.
-     */
-    public boolean isEmpty() {
-        return (currentSize == 0);
-    }
-
-    /*
-     * create enqueue method and pass the parameter
-     * Add data to the end of the list.
-     */
-    public void enqueue(int data) {
-        Node oldRear = rear;
-        rear = new Node();
-        rear.data = data;
-        rear.next = null;
-
-        if (isEmpty()) {
-            front = rear;
-        } else {
-            oldRear.next = rear;
+        /*
+         * class to define linked node
+         */
+        private class Node {
+            int data;
+            Node next;
         }
-        currentSize++;
-        System.out.println(data + " added to the queue");
+
+        // create constructor name as QueueUsingLinkedListMain.
+        public QueueUsingLinkedListMain() {
+
+            front = null;
+            rear = null;
+            currentSize = 0;
+        }
+        /*
+         * create a method isEmpty
+         * they are boolean type that means Returns true if the queue is empty, else false.
+         */
+        public boolean isEmpty() {
+            return (currentSize == 0);
+        }
+
+        /*
+         * create a method name as dequeue.
+         * Remove item from the beginning of the list.
+         */
+        public int dequeue() {
+            int data = front.data;
+            front = front.next;
+            if (isEmpty()) {
+                rear = null;
+            }
+            currentSize--;
+            System.out.println(data + " removed from the queue");
+            return data;
+        }
+
+        /*
+         * create enqueue method and pass the parameter
+         * Add data to the end of the list.
+         */
+        public void enqueue(int data) {
+            Node oldRear = rear;
+            rear = new Node();
+            rear.data = data;
+            rear.next = null;
+
+            if (isEmpty()) {
+                front = rear;
+            } else {
+                oldRear.next = rear;
+            }
+            currentSize++;
+            System.out.println(data + " added to the queue");
+        }
+
+
+        public static void main(String a[]) {
+
+            QueueUsingLinkedListMain queue = new QueueUsingLinkedListMain();
+
+            queue.enqueue(56);
+            queue.dequeue();
+            queue.enqueue(30);
+            queue.dequeue();
+            queue.enqueue(70);
+            queue.dequeue();
+            System.out.println("queue is empty");
+        }
     }
 
-    public static void main(String a[]) {
-
-
-        QueueUsingLinkedListMain queue = new QueueUsingLinkedListMain();
-
-        queue.enqueue( 56);
-        queue.enqueue(30);
-        queue.enqueue(70);
-
-    }
-}
